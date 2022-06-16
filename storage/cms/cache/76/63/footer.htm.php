ff7a8726481f83a1fc8eed8f985d14a819f07959
Anatoliy111 <bondyuk.a.g@gmail.com>
2022-06-16 16:53:59 +0300

    commit

--- storage/cms/cache/76/63/footer.htm.php
+++
@@ -1,5 +0,0 @@
-<?php 
-class Cms6283750e13c49621120705_df7d9e5bd0cabaee0611622e81831640Class extends Cms\Classes\PartialCode
-{
-
-}
--- storage/cms/cache/94/3a/home.htm.php
+++
@@ -1,5 +0,0 @@
-<?php 
-class Cms6283750dee7b7749610587_572a2c039c4f89bc2aba45b0b695480bClass extends Cms\Classes\PageCode
-{
-
-}
